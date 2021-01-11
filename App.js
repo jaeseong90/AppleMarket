663f664472dcda6b24fae5c3b035c4971a714235
unknown <woavldjs12@gmail.com>
2021-01-11 17:51:39 +0900

    aates

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
+import AppLoading from 'expo';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [isReady, setIsReady] = useState(true);
+
+  const loadingFinish = () => {
+    setIsReady(true);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>aaaaaa</Text>
       <StatusBar style="auto" />
     </View>
   );
